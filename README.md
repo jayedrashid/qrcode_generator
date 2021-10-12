997e6c778f7f9dbfdb48501ae935a8b38bb68dcf
Jayed Mohamed Rashid <jayed.dev@gmail.com>
2021-10-12 10:58:16 +0600

    added all the files again by updated a few lines

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# qrcode_generator
-Generate QRcode API
+# QRcode Generator
+QRcode Generator converts any text into an unique qr code. It works through QRcode API.
+
+## Screenshots
+
+<img src="qrcode_generator-screenshot.png">
+  
+## Features
+
+- Use of API.
--- script.js
+++ script.js
@@ -13,17 +13,3 @@ function qrcodeGen() {
 
 
 
-
-
-// (()=>{
-//     const btn = document.querySelector('.submit');
-//     btn.addEventListener('click', qrcodeGen);
-//     function qrcodeGen() {
-//         const qr_text = document.getElementById('qr_text').value;
-//         const qr_width = document.getElementById('qr_width').value;
-//         const qr_height = document.getElementById('qr_height').value;
-//         const url = `https://chart.apis.google.com/chart?cht=qr&chl=${qr_text}&chs=${qr_width}x${qr_height}`;
-//         document.getElementById('qr_img').src = url;
-//     }
-    
-//     })();
